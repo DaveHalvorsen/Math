b58fe6fd109dee8b18052e6b9d09e3a81fbd0e08
David Halvorsen <daviqd.halvorsen@gmail.com>
2018-02-06 15:22:55 -0800

    Create Multiples_of_3_and_5.py

---
+++ Multiples_of_3_and_5.py
@@ -0,0 +1,24 @@
+# This is Project Euler Problem 1: Multiples of 3 and 5 solved in Python 3.6.3.
+# The task is to find the sum of all the multiples of 3 or 5 below 1000.
+
+# current_sum is the current total of the sums. It needs to be defined before being used. 
+current_sum = 0
+# This for loop runs a full cycle of 0 - 1000. Note that 1001 will not be run.
+for i in range(1001):
+    # You'll see 5 #commented out print lines. Those were used for code testing initially. 
+    #print("The current i is: " + str(i))
+    # The modulo operator is used to find if the current i is divisible by 3 
+    if i % 3 == 0: 
+        #print(str(i) + " is divisible by " + str(3))
+        # The current i (that was divisible) is added to the current_sum.
+        current_sum = current_sum + i
+        #print("The current sum is: " + str(current_sum))
+    # The modulo operator is used to find if the current i is divisible by 3 
+    elif i % 5 == 0:
+        #print(str(i) + " is divisible by " + str(5))
+        # The current i (that was divisible) is added to the current_sum.
+        current_sum = current_sum + i
+        #print("The current sum is: " + str(current_sum))
+#This final print statement only prints out the grandtotal.
+print("The current sum is: " + str(current_sum))
+
